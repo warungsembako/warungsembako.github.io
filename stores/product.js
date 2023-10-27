d501125bbf1517af98e98f9799b2f3d1a17e3784
fauziahhennihasibuann <114493159+fauziahhennihasibuann@users.noreply.github.com>
2023-10-27 22:01:27 +0700

    tambah update

---
+++ stores/product.js
@@ -0,0 +1,119 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
+export const useProductStore = defineStore('product', {
+    state: () => ({
+        _products: [],
+        _product: null,
+        addErrors: [],
+        filter: 'terbaru', // Default filter
+        filterCat: 'default',
+        kind: 'date',
+    }),
+
+    getters: {
+        products: (state) => {
+            console.log(state._products)
+            if (state.kind == 'date') {
+                if (state.filter === 'terbaru') {
+                    return state._products // Mengembalikan data terbaru
+                } else if (state.filter === 'terlama') {
+                    return [...state._products].reverse() // Mengembalikan data terlama
+                }
+            } else {
+                return state._products.filter(
+                    (item) => item.relationships.category.id == state.filterCat || state.filterCat == 'default'
+                )
+            }
+        },
+        product: (state) => state._product,
+        errors: (state) => state.addErrors,
+    },
+
+    actions: {
+        async getToken() {
+            await axios.get('/sanctum/csrf-cookie')
+        },
+        async getProducts() {
+            await this.getToken()
+            const data = await axios.get('/api/product')
+            this._products = data.data.data
+        },
+        async getProductById(id) {
+            await this.getToken()
+            await axios.get(`/api/product/${id}`).then((res) => {
+                this._product = res.data.data
+            })
+        },
+        async handleAddData(data) {
+            this.addErrors = []
+            await this.getToken()
+            try {
+                await axios.post('/api/product', {
+                    unit_id: data.unit.id,
+                    category_id: data.category.id,
+                    name: data.name,
+                    desc: data.desc,
+                    price_buy: data.price_buy,
+                    price_sell: data.price_sell,
+                    qty: data.qty,
+                })
+
+                // Menampilkan pemberitahuan SweetAlert2 setelah berhasil
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Sukses!',
+                    text: 'Data produk telah berhasil disimpan ke database.',
+                })
+
+                if (this.addErrors.length == 0) {
+                    this.router.push({ name: 'data-produk' })
+                }
+            } catch (error) {
+                if (error.response.status === 422) {
+                    this.addErrors = error.response.data.errors
+                } else if (error.response.status === 409) {
+                    // Konflik: produk sudah ada
+                    this.addErrors.product = ['Produk sudah ada dalam database.']
+                }
+            }
+        },
+        async handleUpdateData(data, id) {
+            this.addErrors = []
+            await this.getToken()
+            try {
+                await axios
+                    .put(`/api/product/${id}`, {
+                        unit_id: data.unit.id,
+                        category_id: data.category.id,
+                        name: data.name,
+                        desc: data.desc,
+                        price_buy: data.price_buy,
+                        price_sell: data.price_sell,
+                        qty: data.qty,
+                    })
+                    .then(this.router.push({ name: 'data-produk' }))
+            } catch (error) {
+                if (error.response.status === 422) {
+                    this.addErrors = error.response.data.errors
+                }
+            }
+        },
+        async handleDeleteData(id) {
+            await this.getToken()
+            await axios.get(`/api/productDelete/${id}`).then(() => {
+                const productIndex = this.products.findIndex((p) => p.id === id)
+                this.products.splice(productIndex, 1)
+            })
+        },
+        setFilter(kind, filterType) {
+            this.kind = kind
+            this.filter = filterType
+        },
+        setFilterCategory(kind, filterType) {
+            this.kind = kind
+            this.filterCat = filterType
+        },
+    },
+})
