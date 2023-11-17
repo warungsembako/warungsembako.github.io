1caa3fb0b583bb7cc113213d588f34517f92938f
fauziahhennihasibuann <z8958226@gmail.com>
2023-11-17 20:10:23 +0700

    update

---
+++ src/Charts/FooterBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { containerMaxW } from '@/config.js'
+import BaseLevel from '@/components/BaseLevel.vue'
+
+const year = new Date().getFullYear()
+</script>
+
+<template>
+    <footer class="py-2 px-6 fixed bg-white bottom-0 w-full" :class="containerMaxW">
+        <BaseLevel>
+            <div class="text-center md:text-left">
+                <b>&copy;{{ year }}</b>
+            </div>
+        </BaseLevel>
+    </footer>
+</template>
